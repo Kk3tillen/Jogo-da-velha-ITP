98f9f55e965bb449084cbe014136df1ef2bc46c7
Theo Henrique <theo123b@gmail.com>
2025-01-03 20:51:00 -0300

    Feita a verificacao de vitoria

--- src/main.c
+++ src/main.c
@@ -1,23 +1,34 @@
-#include <stdio.h>
-
-
 void verificarPosicao() {
-    
+
 }
 
 
-void verificarVitoria() {
-    
-}
-
-void imprimirMatriz(char matriz[3][3]) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%c ", matriz[i][j]);  
-        }
-        printf("\n");  
+int verificarVitoria(char matriz[3][3], int *win) {
+  for (int i = 0; i < 3; i++) {
+    if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2] || matriz[0][i] == matriz[1][i] && matriz[1][i] == matriz[2][i]){
+      *win = 1;
+      return *win;
+      //return printf("O jogador %c venceu\n", escolhaJogador);
     }
+    else if (matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2] || matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0]){
+      *win = 1;
+      return *win;
+      //return printf("O jogador %c venceu\n", escolhaJogador);
+    } else{
+      return *win;
+    }
+  }
 }
+
+  
+  void imprimirMatriz(char matriz[3][3]) {
+      for (int i = 0; i < 3; i++) {
+          for (int j = 0; j < 3; j++) {
+              printf("%c ", matriz[i][j]);  
+          }
+          printf("\n");  
+      }
+  }
 
 int main() {
     char escolhaJogador;
@@ -48,18 +59,26 @@ int main() {
 
         if(posicao > 0 && posicao <= 3){
             matriz[0][posicao-1] = escolhaJogador;
+            verificarVitoria(matriz, &ganhou);
         } else if(posicao > 3 && posicao <= 6){
             matriz[1][posicao-4] = escolhaJogador;
+            verificarVitoria(matriz, &ganhou);
         } else if (posicao > 6 && posicao <= 9){
             matriz[2][posicao-7] = escolhaJogador;
+            verificarVitoria(matriz, &ganhou);
+            
         } else {
             printf("Posição invalida, escolha um valor entre 1 e 9 :(\n");
         }
 
         imprimirMatriz(matriz);
+        if (ganhou == 1){
+          printf("O jogador %c venceu\n", escolhaJogador);
+        }
+          
+        }
 
         printf("\n");
-    }
-    
+
     return 0;
 }
